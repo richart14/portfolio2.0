a961a123158dfd0ddc217a3b748d048bf483dd73
Richard Tom <tomrichard14@gmail.com>
2018-12-20 14:08:03 -0800

    pane functionality working

---
+++ src/components/Badge.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Badge extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Badge
