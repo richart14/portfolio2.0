145e3f84bbbf525dbef79b64f8c28abc539dc293
Richard Tom <tomrichard14@gmail.com>
2019-01-04 20:37:59 -0800

    updated stack and fixed some bugs with panes

--- src/components/PaneContainer.js
+++ src/components/PaneContainer.js
@@ -11,19 +11,19 @@ export class PaneContainer extends Component {
       <div className="pane-container">
         <Pane 
           id={p1} 
-          togglePane={e => {toggleP1();}}
+          togglePane={e => toggleP1()}
         />
         <Pane 
           id={p2} 
-          togglePane={e => {toggleP2();}}
+          togglePane={e => p2.status === "clear" ? null : toggleP2()}
         />
         <Pane 
           id={p3}
-          togglePane={e => {toggleP3();}}
+          togglePane={e => p3.status === "clear extend" ? null : toggleP3()}
         />
         <Pane 
           id={p4}
-          togglePane={e => {toggleP4();}}
+          togglePane={e => p4.status === "shutter" ? null : toggleP4()}
         />
         <Square shake={square.shake} toggleLand={e => {
           toggleShake(); 
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
 
 export class Stack extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stack: [
+        { name : 'css3', icon : 'fab fa-css3' },
+        { name : 'react', icon : 'fab fa-react' },
+        { name : 'javascript', icon : 'fab fa-js' },
+        
+      ]
+    }
+  }
   render() {
 
     return (
