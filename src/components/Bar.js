6307b8269d788d54906533e2edddd5d5cb2e70fa
Richard Tom <tomrichard14@gmail.com>
2019-01-17 15:32:49 -0800

    added icons to the stack page, still need sliding annimation on hover

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -6,16 +6,19 @@ export class Bar extends Component {
     return (
       <div className={`container ${status.active ? '' : 'hide'}`}>
       
-        <div className="bar"><div className="inner"><span>CSS3</span></div></div>
-        <div className="bar"><div className="inner"><span>HTML5</span></div></div>
-        <div className="bar"><div className="inner"><span>JAVASCRIPT</span></div></div>
-        <div className="bar"><div className="inner"><span>JAVA</span></div></div>
-        <div className="bar"><div className="inner"><span>REACT</span></div></div>
-        <div className="bar"><div className="inner"><span>REDUX</span></div></div>
-        <div className="bar"><div className="inner"><span>NODE.JS</span></div></div>
-        <div className="bar"><div className="inner"><span>EXPRESS</span></div></div>
-        <div className="bar"><div className="inner"><span>MONGODB</span></div></div>
-        <div className="bar"><div className="inner"><span>SQL</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-css3-plain-wordmark"></i><span>CSS3</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-html5-plain-wordmark"></i><span>HTML5</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-javascript-plain"></i><span>JAVASCRIPT</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-java-plain-wordmark"></i><span>JAVA</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-react-original-wordmark"></i><span>REACT</span></div></div>
+        {/* <div className="bar"><div className="inner"><span>REDUX</span></div></div> */}
+        <div className="bar"><div className="inner"><i class="devicon-heroku-original-wordmark"></i><span>HEROKU</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-jquery-plain-wordmark"></i><span>JQUERY</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-nodejs-plain-wordmark"></i><span>NODE.JS</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-express-original-wordmark"></i><span>EXPRESS</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-mongodb-plain-wordmark"></i><span>MONGODB</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-mysql-plain-wordmark"></i><span>SQL</span></div></div>
+        <div className="bar"><div className="inner"><i class="devicon-mocha-plain"></i><span>MOCHA</span></div></div>
       </div>
     )
   }
